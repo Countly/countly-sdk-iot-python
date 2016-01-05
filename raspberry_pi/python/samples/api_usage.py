c997d0b3e319e169263de70ebfdd658877e6bcd0
muharremtac <muharremtac@gmail.com>
2016-01-05 23:40:02 +0200

    Package reorganization

--- raspberry_pi/python/samples/api_usage.py
+++ raspberry_pi/python/samples/api_usage.py
@@ -1,8 +1,8 @@
-import Countly
+from raspberry_pi.python.sdk.Countly import Countly
 
 temperature = 30
 light=50
 
 countly = Countly("try.count.ly", "APP_KEY", "DEVICE_ID", 10)
-countly.event("Temperature",temperature)
+countly.event('Temperature', temperature)
 countly.event("Ligth", light)
