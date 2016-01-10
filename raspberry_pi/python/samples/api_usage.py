52d5de7f6c3c3364751de976ee4d534f92a9e77a
muharremtac <muharremtac@gmail.com>
2016-01-10 13:33:08 +0200

    Fix syntax

--- raspberry_pi/python/samples/api_usage.py
+++ raspberry_pi/python/samples/api_usage.py
@@ -5,4 +5,4 @@ light=50
 
 countly = Countly("try.count.ly", "4b35a3ec6edd429270806f53df6b1cde85cdbf48", "f46a1cfab72a11e5a9e734363bd15286", 20)
 countly.event('Temperature', temperature)
-countly.event("Ligth", light)
+countly.event("Light", light)
