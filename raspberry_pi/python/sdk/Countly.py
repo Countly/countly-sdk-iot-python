e7579147796cde928e6c0b70a26348e642527a91
muharremtac <muharremtac@gmail.com>
2016-01-10 13:25:07 +0200

    Queue imlementation

--- raspberry_pi/python/sdk/Countly.py
+++ raspberry_pi/python/sdk/Countly.py
@@ -1,16 +1,22 @@
 import httplib
 import json
-import threading
-import time
+import locale
+import platform
 import urllib
 
-from system_info import system_info
+import time
+
+import threading
+import uuid
 
 urlStr=""
 appKey=""
 deviceId=""
 periodSecond=0
 connectionStr= ""
+events_queue_file = "events.txt"
+metrics_queue_file = "metrics.txt"
+uuid_file = "uuid.txt"
 
 class Countly:
 
@@ -18,42 +24,40 @@ class Countly:
         global urlStr,appKey,deviceId,periodSecond,connectionStr
         urlStr=url_str
         appKey=app_key
-        deviceId=device_id
+        if(device_id):
+            deviceId=device_id
+        else:
+            deviceId=self.get_device_id()
         periodSecond=period_second
-        connectionStr = httplib.HTTPSConnection(urlStr,443)
+
 
     def init(self):
         self.init(1)
 
     def init(self,start_thrad):
         if start_thrad==1:
-            threading.Timer(periodSecond, self.init).start()
+            thread= threading.Timer(periodSecond, self.init).start()
         metrics = {
               "_os": "Linux",
-              "_os_version": system_info.os_version(),
-              "_device": "Raspberry Pi 2",
+              "_os_version": str(platform.platform()),
+              "_device": platform.machine(),
               "_app_version": "0.0.1",
-              "_locale": system_info.locale()
+              "_locale": locale.getdefaultlocale()
             }
 
         reqmetrics= urllib.quote(json.dumps(metrics))
+        self.write_file(metrics_queue_file, reqmetrics)
+        queuemetrics=self.get_queue_text(metrics_queue_file)
 
-        metrics_file = 'metrics.txt'
-        with open(metrics_file, 'a+') as outfile:
-            json.dump(reqmetrics, outfile)
-            outfile.write('\n')
+        if len(queuemetrics) >2:
+          for queue in queuemetrics:
+            getrequest_init="https://"+urlStr + "/i?begin_session=1&app_key=" + appKey + "&device_id=" + deviceId + "&metrics=" + queue.strip()
+            self.connection_response(getrequest_init, metrics_queue_file)
 
-        with open(metrics_file) as inputfile:
-            reqmetrics_from_file = inputfile.readlines()
-
-
-        getrequest_init="https://"+urlStr + "/i?begin_session=1&app_key=" + appKey + "&device_id=" + deviceId + "&metrics=" + reqmetrics
-
-        connectionStr.request("GET", getrequest_init)
-        response = connectionStr.getresponse()
-        print response.status, response.reason
-        data = response.read()
-        print data
+        else:
+          getrequest_init="https://"+urlStr + "/i?begin_session=1&app_key=" + appKey + "&device_id=" + deviceId + "&metrics=" + queuemetrics[0].strip()
+          self.connection_response(getrequest_init, metrics_queue_file)
+          threading.Timer(periodSecond, self.init).start()
 
     def event(self,key):
         self.init(0)
@@ -65,8 +69,15 @@ class Countly:
             "timestamp": int(time.time())
         }]
         reqevents=urllib.quote(json.dumps(events))
-        self.send_events(reqevents)
-
+        self.write_file(events_queue_file, reqevents)
+        queueevent=self.get_queue_text(events_queue_file)
+        if len(queueevent) >2:
+             for queue in queueevent:
+               getrequest_events="https://"+urlStr + "/i?end_session=1&session_duration=" + str(periodSecond) + "&app_key=" + appKey + "&device_id=" + deviceId + "&events="+queue.strip()
+               self.connection_response(getrequest_events, events_queue_file)
+        else:
+            getrequest_events="https://"+urlStr + "/i?end_session=1&session_duration=" + str(periodSecond) + "&app_key=" + appKey + "&device_id=" + deviceId + "&events="+queueevent[0].strip()
+            self.connection_response(getrequest_events, events_queue_file)
 
 
     def event(self, key, sum_int):
@@ -80,13 +91,74 @@ class Countly:
             "sum": sum_int
         }]
         reqevents=urllib.quote(json.dumps(events))
-        self.send_events(reqevents)
 
-    def send_events(self,reqevents):
-        getrequest_events="https://"+urlStr + "/i?end_session=1&session_duration=" + str(periodSecond) + "&app_key=" + appKey + "&device_id=" + deviceId + "&events="+reqevents
-        connectionStr.request("GET", getrequest_events)
-        response = connectionStr.getresponse()
-        print response.status, response.reason
-        data = response.read()
-        print data
-        connectionStr.close()
+        self.write_file(events_queue_file, reqevents)
+        queueevent=self.get_queue_text(events_queue_file)
+        if len(queueevent) >2:
+            for queue in queueevent:
+               getrequest_events="https://"+urlStr + "/i?end_session=1&session_duration=" + str(periodSecond) + "&app_key=" + appKey + "&device_id=" + deviceId + "&events="+queue.strip()
+               self.connection_response(getrequest_events, events_queue_file)
+        else:
+            getrequest_events="https://"+urlStr + "/i?end_session=1&session_duration=" + str(periodSecond) + "&app_key=" + appKey + "&device_id=" + deviceId + "&events="+queueevent[0].strip()
+            self.connection_response(getrequest_events, events_queue_file)
+
+
+    def connection_response(self, getrequest_url, filename):
+        try:
+            connectionStr = httplib.HTTPSConnection(urlStr,443)
+            connectionStr.request("GET", getrequest_url)
+            response = connectionStr.getresponse()
+            print response.status, response.reason
+            data = response.read()
+            print data
+            connectionStr.close()
+            self.delete_last_line_from_file(filename);
+        except Exception, e:
+           print str(e)
+
+
+    def write_file(self, filename, text):
+        with open(filename, 'a+') as outfile:
+            outfile.write(text+'\n')
+        outfile.close()
+
+    def read_file(self,filename):
+        with open(filename) as readfile:
+           return readfile.readlines()
+
+    def delete_last_line_from_file(self, filename):
+        readFile = open(filename)
+        lines = readFile.readlines()
+        readFile.close()
+        w = open(filename,'w')
+        w.writelines([item for item in lines[:-1]])
+        w.close()
+
+    def get_device_id(self):
+        device_id=""
+        try:
+            device_id=self.read_file(uuid_file)[0].strip()
+            if(device_id):
+                return device_id
+            else:
+                device_id = self.write_device_id(device_id)
+        except Exception:
+            device_id = self.write_device_id(device_id)
+        return device_id
+
+    def write_device_id(self, device_id):
+        self.write_file(uuid_file, str(uuid.uuid1()).replace("-", ""))
+        device_id = self.read_file(uuid_file)[0].strip()
+        return device_id
+
+    def get_queue_text(self, filename):
+        queuetext=""
+        try:
+            queuetext=self.read_file(filename)
+            if(queuetext):
+                return queuetext
+            else:
+                queuetext=""
+        except Exception:
+            queuetext=""
+        return queuetext
