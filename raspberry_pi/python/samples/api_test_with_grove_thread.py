027e66ef5c1859b6637acc16b9e803190e87eb72
muharremtac <muharremtac@gmail.com>
2016-01-15 20:58:19 +0200

    Grove threading timer

--- raspberry_pi/python/samples/api_test_with_grove_thread.py
+++ raspberry_pi/python/samples/api_test_with_grove_thread.py
@@ -1,14 +1,15 @@
 import grovepi
-import time
-
+import threading
 from raspberry_pi.python.sdk.Countly import Countly
-
 
 temperature_sensor = 1
 light_sensor = 0
 
 countly = Countly("try.count.ly", "APP_KEY", 0)
-while True:
-    countly.event('Temperature', grovepi.temp(temperature_sensor,'1.1'))
-    countly.event("Light", int(grovepi.analogRead(light_sensor)/10.24))
-    time.sleep(10)
+
+def grove():
+        threading.Timer(30,grove).start()
+        countly.event('Temperature', grovepi.temp(temperature_sensor,'1.1'))
+        countly.event("Light", int(grovepi.analogRead(light_sensor)/10.24))
+
+grove()
