46fece53acc126d429211d9bffc135204dd4be5f
Görkem Çetin <gc@count.ly>
2016-01-16 01:49:28 +0200

    Update api_test_with_grove.py

--- raspberry_pi/python/samples/api_test_with_grove.py
+++ raspberry_pi/python/samples/api_test_with_grove.py
@@ -5,6 +5,6 @@ from raspberry_pi.python.sdk.Countly import Countly
 temperature_sensor = 1
 light_sensor = 0
 
-countly = Countly("try.count.ly", "APP_KEY", 5)
+countly = Countly("SERVER_URL", "APP_KEY", 5)
 countly.event('Temperature', grovepi.temp(temperature_sensor,'1.1'))
 countly.event("Light", int(grovepi.analogRead(light_sensor)/10.24))
