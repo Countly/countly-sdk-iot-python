fe2aaaa6a918a4e7e4a3862b048cd8c249ab087f
muharremtac <muharremtac@gmail.com>
2016-01-19 21:42:14 +0200

    Tagged version

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'raspberry_pi.python.sdk.Countly',
+  packages = ['raspberry_pi.python.sdk.Countly'],
+  version = '0.1',
+  description = 'Countly IOT SDK from Raspberry Pi Python',
+  author = 'Countly',
+  author_email = 'hello@count.ly',
+  url = 'https://github.com/Countly/countly-sdk-iot',
+  download_url = 'https://github.com/Countly/countly-sdk-iot/raspberry_pi/tarball/0.1',
+  keywords = ['countly', 'iot', 'pyhon'],
+  classifiers = [],
+)
