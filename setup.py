c631d9309dbd4fb97d111d394f193a7e2f1a2d68
muharremtac <muharremtac@gmail.com>
2016-01-19 21:43:07 +0200

    Tagged version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
   name = 'raspberry_pi.python.sdk.Countly',
   packages = ['raspberry_pi.python.sdk.Countly'],
   version = '0.1',
-  description = 'Countly IOT SDK from Raspberry Pi Python',
+  description = 'Countly IOT SDK for Raspberry Pi Python',
   author = 'Countly',
   author_email = 'hello@count.ly',
   url = 'https://github.com/Countly/countly-sdk-iot',
