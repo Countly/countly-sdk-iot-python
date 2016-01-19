c97f344b473b3aa8974ce1ff23abd5c9c2afff95
muharremtac <muharremtac@gmail.com>
2016-01-19 22:51:01 +0200

    Pip installation

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
-  name = 'raspberry_pi.python.sdk.Countly',
-  packages = ['raspberry_pi.python.sdk.Countly'],
+  name = 'raspberry_pi.python.sdk',
+  packages = ['raspberry_pi.python.sdk'],
   version = '0.1',
   description = 'Countly IOT SDK for Raspberry Pi Python',
   author = 'Countly',
   author_email = 'hello@count.ly',
-  url = 'https://github.com/Countly/countly-sdk-iot',
-  download_url = 'https://github.com/Countly/countly-sdk-iot/raspberry_pi/tarball/0.1',
+  url = 'https://github.com/countly/countly-sdk-iot',
+  download_url = 'https://github.com/countly/countly-sdk-iot/tarball/0.1',
   keywords = ['countly', 'iot', 'pyhon'],
   classifiers = [],
 )
