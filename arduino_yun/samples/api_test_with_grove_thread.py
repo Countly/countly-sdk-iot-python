25f42b713f56a74e96b3d2688f0e8994469f3cdf
muharremtac <muharremtac@gmail.com>
2016-03-23 12:01:48 +0200

    Path settings for command prompt starting

--- arduino_yun/samples/api_test_with_grove_thread.py
+++ arduino_yun/samples/api_test_with_grove_thread.py
@@ -1,3 +1,8 @@
+import sys
+import os.path
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
+
 import threading
 
 import grovepi
--- arduino_yun/samples/api_usage.py
+++ arduino_yun/samples/api_usage.py
@@ -1,3 +1,8 @@
+import sys
+import os.path
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
+
 from common.Countly import Countly
 
 temperature = 30
